2a2241d71505d598c04cadd620bec4bd189cdcd7
Vukovic Emilie <emilievukovic79@gmail.com>
2021-06-03 01:28:13 +0200

    Integration HTML CSS (except cards)

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// console.log(recipes);
+// console.log(recipes[2]);
+// console.log(recipes[2].ingredients);
+
+// let eachIngredient;
+
+// recipes.forEach(recipe => {
+// 	const recipesNames = recipe.name;
+// 	console.log(recipesNames);
+
+// 	const allIngredientsRecipes = recipe.ingredients;
+// 	console.log(allIngredientsRecipes);
+
+// 	let eachIngredient = allIngredientsRecipes.map(item => item.ingredient);
+// 	console.log(eachIngredient);
+// });
+
+// let allIngredientList = [];
+// allIngredientList.push(eachIngredient);
+// console.log(allIngredientList);
+
+// const allIngredientList = recipes.map(item => item.ingredient);
+// console.log(allIngredientList);
